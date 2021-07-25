953215c2df7c5a1d640f0ddc9f3ce9a24ada3db0
1gni5 <1gni5@net-c.com>
2021-07-25 23:42:35 +0200

    Creates Flashcard model.

--- src/flashcards/models.py
+++ src/flashcards/models.py
@@ -1,3 +1,21 @@
+from datetime import timedelta
 from django.db import models
 
-# Create your models here.
+# Flashcard model
+class Flashcard(models.Model):
+    """
+    Simple flashcard representation with a pair question/answer.
+    And informations for SM2 algorithm (repetition, easiness_factor
+    and interval).
+    """
+
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+    # Internal fields
+    repetitions = models.PositiveIntegerField(default=0)
+    easiness_factor = models.FloatField(default=2.5) # SM2 specification
+    interval = models.DurationField(default=timedelta(days=1))
+
+    def __str__(self):
+        return f'{self.question}'
