72ffff216d57602fb1921958ae133ec386b1936b
1gni5 <1gni5@net-c.com>
2021-07-26 11:10:39 +0200

    Add constants from SM2 algorithm.

--- src/flashcards/models.py
+++ src/flashcards/models.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 from django.db import models
 
+SM2_DEFAULT_INTERVAL = timedelta(days=1)
+
 # Flashcard model
 class Flashcard(models.Model):
     """
@@ -15,7 +17,40 @@ class Flashcard(models.Model):
     # Internal fields
     repetitions = models.PositiveIntegerField(default=0)
     easiness_factor = models.FloatField(default=2.5) # SM2 specification
-    interval = models.DurationField(default=timedelta(days=1))
+    interval = models.DurationField(default=SM2_DEFAULT_INTERVAL)
 
     def __str__(self):
         return f'{self.question}'
+
+    def update(self, feedback):
+        """
+        Updates current flashcard's infos using user feedback 
+        and SM2 algorithm.
+        """
+
+        # Is given answer correct ?
+        if feedback >= 3:
+
+            # Interval
+            if self.repetitions == 0:
+                self.interval = SM2_DEFAULT_INTERVAL
+            elif self.repetitions == 1:
+                self.interval = timedelta(days=6)
+            else:
+                self.interval *= self.easiness_factor
+
+            # Easiness factor
+            self.easiness_factor += 0.1 - (5 - feedback) * \
+                    (0.08 + (5 - feedback) * 0.02)
+
+            if self.easiness_factor <= 1.3: # SM2 specification
+                self.easiness_factor = 1.3
+
+            # Repetitions
+            self.repetitions += 1
+
+        else:
+
+            # Reset everything
+            self.repetitions = 0
+            self.interval = SM2_DEFAULT_INTERVAL
